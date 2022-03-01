d1d13fc31179a0734461679f026c15ea06238ac8
Vitaliy Biryuckov <stukov.biryuckov2017@yandex.ru>
2022-03-01 18:20:34 +0800

    Update step0 script some comments and use f-string
    Update all strings (uncludes file writing) from str.format to f-strings

--- AAAI19/exp_T2D/evaluate_infer_col_gt.py
+++ AAAI19/exp_T2D/evaluate_infer_col_gt.py
@@ -1,30 +1,46 @@
-"""
-This file contains a script that
-extracts column ground truth classes of columns of T2Dv2
-output: column_gt_extend_fg.csv (best class + okay classes of each column)
-"""
 import os
 import sys
 import argparse
 from util_kb import super_classes
 from util_t2d import read_col_gt
 
-current_path = os.path.dirname(os.path.realpath(sys.argv[0]))
-parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    '--io_dir',
-    type=str,
-    default=os.path.join(current_path, 'in_out'),
-    help='Directory of input/output')
-FLAGS, unparsed = parser.parse_known_args()
+"""
+This file contains a script that
+extracts column ground truth classes of columns of T2Dv2
+output: column_gt_extend_fg.csv (best class + okay classes of each column)
+"""
+"""
+Скрипт первого шага. 
+Находит для каждого столбца класс из DbPedia
+Результатом является csv-файл column_gt_extend_fg.csv (Номер таблицы, Лучший класс для столбца + подходящие классы)
+"""
+# -*- coding: utf-8 -*-
 
-col_classes = read_col_gt()
-col_classes = super_classes(col_classes)
+def extract_classes_columns():
+    current_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+    parser = argparse.ArgumentParser()
 
-with open(os.path.join(FLAGS.io_dir, 'column_gt_extend_fg.csv'), 'w') as f:
-    for col in col_classes.keys():
-        classes_str = '"' + '","'.join(col_classes[col]) + '"'
-        f.write('"%s",%s\n' % (col, classes_str))
+    parser.add_argument(
+        '--io_dir',
+        type=str,
+        default=os.path.join(current_path, 'in_out'),
+        help='Directory of input/output')
+    FLAGS, unparsed = parser.parse_known_args()
 
+    # Читаем намера таблиц и их супер классы
+    # Из файла col_class_checked_fg
+    col_classes = read_col_gt()
 
+    # Дополняем этот словарь 'окей' классами из DBPedia
+    col_classes = super_classes(col_classes)
+
+    # Записываем в файл с строгим форматированием
+    with open(os.path.join(FLAGS.io_dir, 'column_gt_extend_fg.csv'), 'w', encoding="utf-8") as f:
+        for col in col_classes.keys():
+            joined_str = '","'.join(col_classes[col])
+            classes_str = f'"{joined_str}"'
+            f.write(f'"{col}", "{classes_str}"\n')
+
+if __name__ =='__main__':
+    extract_classes_columns()
